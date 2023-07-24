0a6a9f014567f8ea24b303758bb9d17786989237
sangyoon kim <sangyun0904@gmail.com>
2023-07-24 22:37:04 +0900

    달력 변경

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,80 @@
 import React, { useState } from "react";
 import fakeData from "./MOCK_DATA.json";
-import Calendar from 'react-calendar';
 import { useTable } from "react-table";
 
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-datepicker/dist/react-datepicker.css";
+
+const locales = {
+    "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events = [
+    {
+        title: "Big Meeting",
+        allDay: true,
+        start: new Date(2021, 6, 0),
+        end: new Date(2021, 6, 0),
+    },
+    {
+        title: "Vacation",
+        start: new Date(2021, 6, 7),
+        end: new Date(2021, 6, 10),
+    },
+    {
+        title: "Conference",
+        start: new Date(2021, 6, 20),
+        end: new Date(2021, 6, 23),
+    },
+];
+
 const Main = () => {
-    const [value, onChange] = useState(new Date());
+
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [allEvents, setAllEvents] = useState(events);
+
+    function handleAddEvent() {
+        
+        for (let i=0; i<allEvents.length; i++){
+
+            const d1 = new Date (allEvents[i].start);
+            const d2 = new Date(newEvent.start);
+            const d3 = new Date(allEvents[i].end);
+            const d4 = new Date(newEvent.end);
+      /*
+          console.log(d1 <= d2);
+          console.log(d2 <= d3);
+          console.log(d1 <= d4);
+          console.log(d4 <= d3);
+            */
+
+             if (
+              ( (d1  <= d2) && (d2 <= d3) ) || ( (d1  <= d4) &&
+                (d4 <= d3) )
+              )
+            {   
+                alert("CLASH"); 
+                break;
+             }
+    
+        }
+        
+        
+        setAllEvents([...allEvents, newEvent]);
+    }
+
     const data = React.useMemo(() => fakeData, []);
     const columns = React.useMemo(
       () => [
@@ -41,9 +111,7 @@ const Main = () => {
 
     return (
         <main>
-            <div>
-                <Calendar onChange={onChange} value={value} />
-            </div>
+            <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
             <div className="container">
                 <table {...getTableProps()}>
                 <thead>
